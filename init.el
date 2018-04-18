71201593dd3aecf8c9e283e02da9b5ddfe79ad5a
Dinesh A <adepu.dinesh.a@gmail.com>
2018-04-18 22:38:25 +0530

    Org mode. Theme.

--- init.el
+++ init.el
@@ -12,23 +12,15 @@
   (package-install 'spacemacs-theme))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (spacemacs-dark)))
- '(custom-safe-themes
-   (quote
-    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
- '(package-selected-packages
-   (quote
-    (exec-path-from-shell yasnippet spaceline company dashboard flx-ido smex org-bullets beacon spacemacs-theme which-key use-package))))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 148 :width normal :foundry "nil" :family "xos4 Terminus")))))
+ )
