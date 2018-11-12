04f5dd15553e81823b4329cf4a84b0cdf07c6221
dineshadepu <adepu.dinesh.a@gmail.com>
2018-11-12 20:41:14 +0530

    Org ref for phd. Yasnippet tab completion.

--- init.el
+++ init.el
@@ -18,10 +18,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(markdown-command "/usr/bin/pandoc" t)
- '(org-agenda-files (quote ("~/Dropbox/PhD/ideas.org")))
+ '(org-agenda-files
+   (quote
+    ("~/phd_thesis/notes/org_ref_manual.org" "~/Dropbox/org/todo.org" "~/Dropbox/org/dem.org")))
  '(package-selected-packages
    (quote
-    (racket-mode ess linum-relative magithub company-reftex org-plus-contrib pyvenv virtualenv elpy yasnippet-snippets which-key use-package toml-mode symon spacemacs-theme spaceline smex rg restart-emacs rainbow-delimiters racer ox-reveal org-ref org-bullets monokai-theme monokai-alt-theme helm-swoop golden-ratio git-timemachine git-gutter flycheck-rust flycheck-package flx-ido exec-path-from-shell evil-terminal-cursor-changer evil-nerd-commenter evil-magit evil-leader evil-escape dmenu diminish dashboard csharp-mode company-statistics company-math company-irony company-auctex color-theme clang-format cargo beacon avy aggressive-indent))))
+    (org-autolist racket-mode ess linum-relative magithub company-reftex org-plus-contrib pyvenv virtualenv elpy yasnippet-snippets which-key use-package toml-mode symon spacemacs-theme spaceline smex rg restart-emacs rainbow-delimiters racer ox-reveal org-ref org-bullets monokai-theme monokai-alt-theme helm-swoop golden-ratio git-timemachine git-gutter flycheck-rust flycheck-package flx-ido exec-path-from-shell evil-terminal-cursor-changer evil-nerd-commenter evil-magit evil-leader evil-escape dmenu diminish dashboard csharp-mode company-statistics company-math company-irony company-auctex color-theme clang-format cargo beacon avy aggressive-indent))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
