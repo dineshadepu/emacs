4ca68e1f43a24627ba4eb7b01270b75510ff2e4a
dineshadepu <adepu.dinesh.a@gmail.com>
2018-11-21 10:34:19 +0530

    Sage math, doom mode line.

--- init.el
+++ init.el
@@ -11,6 +11,7 @@
   (package-refresh-contents)
   (package-install 'spacemacs-theme))
 
+(add-to-list 'default-frame-alist '(background-color . "lightgray"))
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -18,12 +19,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(markdown-command "/usr/bin/pandoc" t)
- '(org-agenda-files
-   (quote
-    ("~/README.org" "~/Dropbox/org/todo.org" "~/Dropbox/org/dem.org")))
  '(package-selected-packages
    (quote
-    (org-autolist racket-mode ess linum-relative magithub company-reftex org-plus-contrib pyvenv virtualenv elpy yasnippet-snippets which-key use-package toml-mode symon spacemacs-theme spaceline smex rg restart-emacs rainbow-delimiters racer ox-reveal org-ref org-bullets monokai-theme monokai-alt-theme helm-swoop golden-ratio git-timemachine git-gutter flycheck-rust flycheck-package flx-ido exec-path-from-shell evil-terminal-cursor-changer evil-nerd-commenter evil-magit evil-leader evil-escape dmenu diminish dashboard csharp-mode company-statistics company-math company-irony company-auctex color-theme clang-format cargo beacon avy aggressive-indent))))
+    (doom-modeline eyebrowse sage-shell-mode yasnippet-snippets writegood-mode which-key virtualenv vimrc-mode use-package toml-mode toc-org tiny spacemacs-theme spaceline smex smartparens rust-playground rg restart-emacs rainbow-delimiters racket-mode racer py-yapf ox-reveal org-ref org-pdfview org-bullets org-autolist ob-ipython nlinum-relative neotree monokai-theme magithub linum-relative irony-eldoc intellij-theme htmlize helm-swoop helm-make helm-fuzzier google-c-style golden-ratio git-timemachine git-gutter ggtags fzf flycheck-rust flycheck-package flycheck-irony flx-ido exec-path-from-shell evil-terminal-cursor-changer evil-nerd-commenter evil-magit evil-leader ess elpy elisp-slime-nav dired-details diminish dashboard counsel-gtags company-statistics company-math company-irony-c-headers company-irony company-c-headers company-auctex color-theme clang-format cdlatex cargo beacon basic-theme avy aggressive-indent))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
